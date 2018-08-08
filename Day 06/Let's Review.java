3c13cb89fd156dfda05da07a3f9b57489646a068
Mohammadreza Ashori <rzalone@gmail.com>
2018-08-08 17:09:37 +0430

    Update Let's Review.java

--- Day 06/Let's Review.java
+++ Day 06/Let's Review.java
@@ -1,1 +1,29 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int numOfString = input.nextInt(); //Read number of string we want enter
+        
+        for(int i = 0; i < numOfString; i++)
+        {
+            char[] inputString = input.next().toCharArray();
+            // Print even chars
+            for(int j = 0; j < inputString.length; j += 2){
+                System.out.print(inputString[j]);
+            }
+            System.out.print(" ");
+            
+            // Print odd chars
+            for(int j = 1; j < inputString.length; j += 2){
+                System.out.print(inputString[j]);
+            }
+            System.out.println();
+        }
+        input.close();
+        
+    }
+}
