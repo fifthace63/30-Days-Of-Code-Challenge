103740250d65f9d7704b815145b38bedd9819be3
Mohammadreza Ashori <rzalone@gmail.com>
2018-08-16 12:04:47 +0430

    Update BinaryNumbers.java

--- Day 10/BinaryNumbers.java
+++ Day 10/BinaryNumbers.java
@@ -1,1 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int remind = 0;
+        int counter = 0;
+        int sequence = 0;
+        
+        while(n > 0)
+        {
+            remind = n%2;
+            n = n/2;
+            if (remind == 1)
+            {
+                counter++;
+                if(counter > sequence)
+                    sequence = counter;
+            }
+            else{
+                counter = 0;
+            }  
+        }
+        System.out.println(sequence);
+
+        scanner.close();
+    }
+}
 
